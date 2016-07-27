d5cd5b8b8b049a43010644d443ca0f489553fa67
H Kluiver <h.kluiver@gmail.com>
2016-07-27 16:18:38 +0200

    Merge JF

---
+++ framework/csCommonSense/Types/DataServer/JoinSharedServiceEventArgs.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csCommon.Types.DataServer
+{
+    public class JoinSharedServiceEventArgs : EventArgs
+    { 
+
+        public JoinSharedServiceEventArgs(string pSharedServiceName, Guid pSharedServiceGuid, int pHostingImbClientId)
+        {
+            SharedServiceName = pSharedServiceName;
+            SharedServiceGuid = pSharedServiceGuid;
+            HostingImbClientId = pHostingImbClientId;
+
+            JoinSharedService = null;
+        }
+
+        public string SharedServiceName { get; private set; }
+        public Guid SharedServiceGuid { get; private set; }
+        public int HostingImbClientId { get; private set; }
+        public bool? JoinSharedService { get; set; }
+    }
+}
