411c72221633173605f750fb96bba1f6f4849387
kluiverjh <hein.kluiver@tno.nl>
2016-08-30 08:58:34 +0200

    Possibility to use excel configuration sheet for config (command line option)

---
+++ framework/csCommonSense/ConfigCmdLineOptions.cs
@@ -0,0 +1,29 @@
+﻿using CommandLine;
+using CommandLine.Text;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csCommon
+{
+    public class ConfigCmdLineOptions
+    {
+        [Option('c', "ConfigurationFile", Required = false, HelpText = "Excel configuration file.")]
+        public string ConfigurationFile { get; set; }
+
+        [Option('n', "ConfigurationName", DefaultValue = "", HelpText = "Configurationname in excel sheet")]
+        public string ConfigurationName { get; set; }
+
+        [ParserState]
+        public IParserState LastParserState { get; set; }
+
+        [HelpOption]
+        public string GetUsage()
+        {
+            return HelpText.AutoBuild(this,
+              (HelpText current) => HelpText.DefaultParsingErrorsHandler(this, current));
+        }
+    }
+}
